e14656aa6c841798ac769303ef7bf147f759e4fe
plusdojo2026-yasudataito <yasuda-taito-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-10 17:36:05 +0900

    新規登録

--- source/src/main/java/servlet/SignupServlet.java
+++ source/src/main/java/servlet/SignupServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UsersDao;
+import dto.User;
 
 /**
  * Servlet implementation class SignupServlet
@@ -13,29 +19,48 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/SignupServlet")
 public class SignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SignupServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらサインアップサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (!(session.getAttribute("user_id") == null)) {
+			response.sendRedirect("/webapp/LoginServlet");
+			return;
+		}
+		// 登録ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/signup.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていたらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (!(session.getAttribute("user_id") == null)) {
+			response.sendRedirect("/webapp/LoginServlet");
+			return;
+		}
 
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String user_id = request.getParameter("user_id");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String mail_address = request.getParameter("mail_address");
+
+		// 登録処理を行う
+		UsersDao UsersDao = new UsersDao();
+		UsersDao.insert(new User(user_id, password, name, mail_address));
+		response.sendRedirect("/webapp/LoginServlet");
+		return;
+	}
 }
--- source/src/main/webapp/WEB-INF/jsp/alert.jsp
+++ source/src/main/webapp/WEB-INF/jsp/alert.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,37 +8,32 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<header class="header">
-<h1 class="logo">Motta?</h1>
-<nav class="nav">
-<ul>
-<li><a href="#">TOP</a></li>
-<li><a href="#">登録</a></li>
-<li><a href="#">一覧</a></li>
-<li><a href="#">検索</a></li>
-<li><a href="#">チェックリスト</a></li>
-<li><a href="#">ログアウト</a></li>
-</ul>
-</nav>
-</header>
-<form class="date-area">
+	<header class="header">
+		<h1 class="logo">Motta?</h1>
+		<nav class="nav">
+			<ul>
+				<li><a href="#">TOP</a></li>
+				<li><a href="#">登録</a></li>
+				<li><a href="#">一覧</a></li>
+				<li><a href="#">検索</a></li>
+				<li><a href="#">チェックリスト</a></li>
+				<li><a href="#">ログアウト</a></li>
+			</ul>
+		</nav>
+	</header>
+	<form class="date-area">
 
-<label>通知日</label>
-<input type="date" name="date">
+		<label>通知日</label>
+			<input type="date" name="date">
+				<label class="time-label">通知時刻</label>
+			<input type="time" name="time">
 
-<label class="time-label">通知時刻</label>
-<input type="time" name="time">
+		<div class="button-area">
+			<button type="submit" class="btn-register">登録</button>
 
-<div class="button-area">
-<button type="submit" class="btn-register">
-登録
-</button>
+			<button type="reset" class="btn-reset">リセット</button>
+		</div>
 
-<button type="reset" class="btn-reset">
-リセット
-</button>
-</div>
-
-</form>
+	</form>
 </body>
 </html>
