a9d3d77e7a10822d61a774d744c85dda5b07a802
plusdojo2026-fujimoto <fujimoto-rui-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-12 16:28:43 +0900

    registの分です。

---
+++ source/src/main/java/dto/Regist.java
@@ -0,0 +1,51 @@
+package dto;
+
+public class Regist {
+
+	private String name; // 名称//
+	private String location; // 発生場所//
+	private String date; // 日付//
+	private String weather; // 天気//
+	private String reason; // 原因//
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getWeather() {
+		return weather;
+	}
+
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+}
