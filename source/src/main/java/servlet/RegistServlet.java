3196d3558eca3eb33272b867840e747d2020ded5
plusdojo2026-fujimoto <fujimoto-rui-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 14:09:45 +0900

    レジスト修正できました

--- source/src/main/java/servlet/RegistServlet.java
+++ source/src/main/java/servlet/RegistServlet.java
@@ -9,10 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.RegistDao;
+import dto.Regist;
+
 /**
  *  * Servlet implementation class MenuServlet  
  */
-@WebServlet("/registServlet")
+@WebServlet("/RegistServlet")
 public class RegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -22,10 +25,33 @@ public class RegistServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		// チェックリストページにフォワードする
+	
+		// ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
 		dispatcher.forward(request, response);
+	}
+		protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		 throws ServletException, IOException {
+
+			// リクエストパラメータを取得する
+			request.setCharacterEncoding("UTF-8");
+			String item_name = request.getParameter("lost_items");
+			String name = request.getParameter("name");
+			String location = request.getParameter("location");
+			String date = request.getParameter("date");
+			String weather = request.getParameter("weather");
+			String reason = request.getParameter("reason");
+			
+			// 登録処理を行う
+			RegistDao bDao = new RegistDao();
+
+			Regist list = new Regist();
+			
+
+			if (bDao.insert(list)) {
+		  response.sendRedirect("/e1/RegistServlet");
+		}
 
 	}
-}
+	}
+
