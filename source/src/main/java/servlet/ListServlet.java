cbf5dc215fad787adb69c09b0db8f29e1e5d3656
plusdojo2026-tsukuma <tsukuma-hanaka-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 14:40:37 +0900

    一覧jsp,css,Servlet修正
    ・一覧画面にキャラクターと分析メッセージ表示させました
    ・おこっちょん、ないちょんの画像ファイル追加しています

--- source/src/main/java/servlet/ListServlet.java
+++ source/src/main/java/servlet/ListServlet.java
@@ -1,7 +1,11 @@
 package servlet;
 
 import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -43,9 +47,81 @@ public class ListServlet extends HttpServlet {
     	// ログインユーザーの忘れ物だけ取得
     	List<LostItems> itemList =
     	        dao.selectAll(userId, sort);
+    	
+    	//曜日の取得
+    	Map<DayOfWeek, Integer> weekCount = new HashMap<>();
+    	for (LostItems item : itemList) {
+    	    LocalDate date =
+    	            LocalDate.parse(item.getLost_date());
+    	    DayOfWeek day =
+    	            date.getDayOfWeek();
+    	    weekCount.put(
+    	            day,
+    	            weekCount.getOrDefault(day, 0) + 1);
+    	}
+    	//　最多曜日を取得
+    	DayOfWeek maxDay = null;
+    	int maxCount = 0;
+    	for (Map.Entry<DayOfWeek, Integer> entry
+    	        : weekCount.entrySet()) {
+    	    if (entry.getValue() > maxCount) {
+    	        maxCount = entry.getValue();
+    	        maxDay = entry.getKey();
+    	    }
+    	}
+    	//コメント生成
+    	String analysisComment;
+
+    	if (maxDay == null) {
+
+    	    analysisComment =
+    	            "まだ分析できるデータがありません";
+
+    	} else {
+
+    	    String dayName = "";
+
+    	    switch (maxDay) {
+
+    	    case MONDAY:
+    	        dayName = "月曜日";
+    	        break;
+
+    	    case TUESDAY:
+    	        dayName = "火曜日";
+    	        break;
+
+    	    case WEDNESDAY:
+    	        dayName = "水曜日";
+    	        break;
+
+    	    case THURSDAY:
+    	        dayName = "木曜日";
+    	        break;
+
+    	    case FRIDAY:
+    	        dayName = "金曜日";
+    	        break;
+
+    	    case SATURDAY:
+    	        dayName = "土曜日";
+    	        break;
+
+    	    case SUNDAY:
+    	        dayName = "日曜日";
+    	        break;
+    	    }
+
+    	    analysisComment =
+    	    		"むむっ！" +
+    	            dayName +
+    	            "に忘れ物が多いようです！";
+    	}
 
     	// 取得した一覧データをJSPへ渡す
     	request.setAttribute("itemList", itemList);
+    	//曜日情報をJSPへ渡す
+    	request.setAttribute("analysisComment", analysisComment);
     	// 現在の並び順もJSPへ渡す
         request.setAttribute("sort", sort);
 
